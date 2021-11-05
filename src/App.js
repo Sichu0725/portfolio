c4ec333aca6e9240b154ba63441e219d8c0c5f0e
Sichu0725 <choihc8511@gmail.com>
2021-11-05 15:08:18 +0900

    생각이 안난다

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import Header from "./components/header";
 
 import './style/index.css'
+
 function App() {
   return (
     <div>
-      <Header/>
+      어떻게 만들지<br/>
+      하하하하하하하핳핳핳핳    
     </div>
   );
 }
